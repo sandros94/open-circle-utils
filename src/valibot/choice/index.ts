f96b23c8163107c33f79c5aab81abffc4a473851
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-01-26 00:39:45 +0100

    feat(valibot): add choice utils

---
+++ src/valibot/choice/index.ts
@@ -0,0 +1,3 @@
+export type * from './types.ts';
+export * from './get.ts';
+export * from './is.ts';
---
+++ src/valibot/choice/is.ts
@@ -0,0 +1,91 @@
+import type {
+  GenericSchema,
+  GenericSchemaAsync,
+} from 'valibot';
+
+import type {
+  GenericEnumSchema,
+  GenericUnionSchema,
+  GenericUnionSchemaAsync,
+  GenericPicklistSchema,
+  GenericVariantSchema,
+  GenericVariantSchemaAsync,
+} from './types.ts';
+
+/**
+ * Check if a schema is an enum schema.
+ *
+ * @param schema The schema to check.
+ *
+ * @returns True if the schema is an enum schema.
+ */
+// @__NO_SIDE_EFFECTS__
+export function isEnumSchema<
+  TSchema extends GenericSchema | GenericSchemaAsync,
+>(
+  schema: TSchema
+): schema is TSchema & GenericEnumSchema {
+  if (!('type' in schema)) return false;
+
+  return schema.type === 'enum';
+}
+
+/**
+ * Check if a schema is a picklist schema.
+ *
+ * @param schema The schema to check.
+ *
+ * @returns True if the schema is a picklist schema.
+ */
+// @__NO_SIDE_EFFECTS__
+export function isPicklistSchema<
+  TSchema extends GenericSchema | GenericSchemaAsync,
+>(
+  schema: TSchema
+): schema is TSchema & GenericPicklistSchema {
+  if (!('type' in schema)) return false;
+
+  return schema.type === 'picklist';
+}
+
+/**
+ * Check if a schema is a union schema.
+ *
+ * @param schema The schema to check.
+ *
+ * @returns True if the schema is a union schema.
+ */
+// @__NO_SIDE_EFFECTS__
+export function isUnionSchema<
+  TSchema extends GenericSchema | GenericSchemaAsync
+>(
+  schema: TSchema
+): schema is TSchema & (
+  | GenericUnionSchema
+  | GenericUnionSchemaAsync
+) {
+  if (!('type' in schema)) return false;
+
+  return schema.type === 'union';
+}
+
+/**
+ * Check if a schema is a variant schema.
+ *
+ * @param schema The schema to check.
+ *
+ * @returns True if the schema is a variant schema.
+ */
+// @__NO_SIDE_EFFECTS__
+export function isVariantSchema<
+  TSchema extends GenericSchema | GenericSchemaAsync,
+>(
+  schema: TSchema
+): schema is TSchema & (
+  | TSchema extends GenericVariantSchema<infer T> ? GenericVariantSchema<T & string> : never
+  | TSchema extends GenericVariantSchemaAsync<infer T> ? GenericVariantSchemaAsync<T & string> : never
+) {
+  if (!('type' in schema)) return false;
+
+  return schema.type === 'variant' && 'key' in schema;
+}

c9ee0b25f55ffe001cdf438663ae9fa7a9179357
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-01-26 20:02:56 +0100

    fix(eslint): update ignores to exclude dist and modules directories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import unjs from "eslint-config-unjs";
 
 export default unjs({
-  ignores: [
-    // ignore paths
-  ],
+  ignores: ["packages/*/dist/**", "packages/*/node_modules/**"],
   rules: {
     "unicorn/no-null": "off",
     "unicorn/no-nested-ternary": "off",

a788fbfa9127c09154868dcdff2fa146e35ca8fb
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-02-18 02:33:17 +0100

    chore: delete formisch-utils implementation

--- packages/formisch-utils/src/index.ts
+++ packages/formisch-utils/src/index.ts
@@ -5,32 +5,3 @@
  *
  * @module formisch-utils
  */
-
-export {
-  generateInitialInput,
-  type GenerateInitialInputOptions,
-
-  // Deprecated exports - will be removed in future versions
-  generateInitialValues,
-  type GenerateInitialValuesOptions,
-} from "./generate-initial-input.js";
-
-export {
-  inferInputType,
-  type InferredInputType,
-  type InferInputTypeOptions,
-  type InferInputTypeResult,
-} from "./infer-input-type.js";
-
-export {
-  inferInputConstraints,
-  type InputConstraints,
-  type InferInputConstraintsOptions,
-  type InferInputConstraintsResult,
-} from "./infer-input-constraints.js";
-
-export {
-  inferLabel,
-  inferDescription,
-  inferPlaceholder,
-} from "./infer-field-info.js";

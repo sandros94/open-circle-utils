2831f6497f742b040e1dbd19937b98f06ba69e19
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-03-01 18:09:44 +0100

    fix(changelog): swap tagMessage and tagBody formats for consistency

--- packages/formisch-utils/changelog.config.ts
+++ packages/formisch-utils/changelog.config.ts
@@ -3,7 +3,7 @@ import type { ChangelogConfig } from "changelogen";
 export default <ChangelogConfig>{
   templates: {
     commitMessage: "chore(release): formisch-utils v{{newVersion}}",
-    tagMessage: "formisch-utils@{{newVersion}}",
-    tagBody: "formisch-utils v{{newVersion}}",
+    tagMessage: "formisch-utils v{{newVersion}}",
+    tagBody: "formisch-utils@{{newVersion}}",
   },
 };
--- packages/valibot-ast/changelog.config.ts
+++ packages/valibot-ast/changelog.config.ts
@@ -3,7 +3,7 @@ import type { ChangelogConfig } from "changelogen";
 export default <ChangelogConfig>{
   templates: {
     commitMessage: "chore(release): valibot-ast v{{newVersion}}",
-    tagMessage: "valibot-ast@{{newVersion}}",
-    tagBody: "valibot-ast v{{newVersion}}",
+    tagMessage: "valibot-ast v{{newVersion}}",
+    tagBody: "valibot-ast@{{newVersion}}",
   },
 };
